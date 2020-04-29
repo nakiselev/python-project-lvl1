44ba19888c082ad573d38d73d01b0c97e2f2c805
Nikita Kiselev <nakisele92@gmail.com>
2020-04-29 15:34:19 +0300

    Add Travis and README

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build Status](https://travis-ci.com/nakiselev/python-project-lvl1.svg?branch=master)](https://travis-ci.com/nakiselev/python-project-lvl1)
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,15 @@
+
+from brain_games.cli import welcome_user
+
+
 def start():
     print('Welcome to the Brain Games!')
+    print('')
+
 
 def main():
     start()
+    welcome_user()
 
 
 if __name__ == '__main__':
